095b448991dadfc11c2d58e75468c22e9d80b8bc
Alex Nichol <alexnichol@comcast.net>
2012-05-19 11:04:59 -0400

    Added *

---
+++ Launch List/ANAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  ANAppDelegate.m
+//  Launch List
+//
+//  Created by Alex Nichol on 5/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ANAppDelegate.h"
+
+@implementation ANAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ LaunchListHelper/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  LaunchListHelper
+//
+//  Created by Alex Nichol on 5/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
